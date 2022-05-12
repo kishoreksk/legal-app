b490b34fe8597a916508ede17526d2e1d248f13f
Kishore KS <kishore.shobana@gmail.com>
2022-05-12 20:33:55 +0530

    Added route path

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'public#index'
   constraints subdomain: /.*/ do
     resources :clients
   end
@@ -6,5 +7,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "index"
 end
